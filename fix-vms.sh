cc8abe2710686bc09b1fbce8f82c29ff25c8c4de
BigAnteater <77298458+BigAnteater@users.noreply.github.com>
2022-03-27 21:26:57 -0700

    Update fix-vms.sh

--- fix-vms.sh
+++ fix-vms.sh
@@ -1,12 +1,6 @@
-if [ $EUID -ne 0 ]
-	then
-		echo "This program must run as root to function." 
-		exit 1
-fi
-
 echo "patching VMs."
 sleep 1s
-pacman -S vlc --noconfirm --quiet
+sudo pacman -S vlc --noconfirm --quiet
 clear
 
 vlc https://youtu.be/dQw4w9WgXcQ
